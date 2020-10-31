4e57250f65fda0b8ad0d4bcdaf64b1ee1b291f38
Mikhail Ilyushin <mishamine26@mail.ru>
2020-10-31 19:24:42 +0300

    Начало реализации корзины

---
+++ KnivesStore.BLL/Interfaces/IBasketService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KnivesStore.BLL.Interfaces
+{
+    public interface IBasketService
+    {
+        
+    }
+}
---
+++ KnivesStore/Controllers/BasketController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KnivesStore.PL.Controllers
+{
+    public class BasketController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
